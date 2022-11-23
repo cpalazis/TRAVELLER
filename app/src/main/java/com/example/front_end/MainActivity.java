d26294a14915a7f0f1b62a935cc9a1fba8d0862a
cpalazis <cpalazis@yahoo.com>
2022-11-23 22:43:02 +0200

    Added SightsActivity

--- app/src/main/java/com/example/front_end/MainActivity.java
+++ app/src/main/java/com/example/front_end/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.front_end;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button Museums;
+        Button Hotels;
+        Button Banks;
+        Button Caffe;
+        Button Photos;
+        Button Restaurants;
+        Button Sights;
+        Button General_Informations;
+
+
+        Museums =findViewById(R.id.button);
+        Hotels =findViewById(R.id.button2);
+        Banks=findViewById(R.id.button3);
+        Caffe=findViewById(R.id.button4);
+        Photos=findViewById(R.id.button5);
+        Restaurants=findViewById(R.id.button6);
+        Sights=findViewById(R.id.button7);
+        General_Informations=findViewById(R.id.button8);
+
+
+        Sights.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent New_Screen=new Intent(getApplicationContext(),SightsActivity.class);
+                startActivity(New_Screen);
+            }
+        });
+
     }
 }
