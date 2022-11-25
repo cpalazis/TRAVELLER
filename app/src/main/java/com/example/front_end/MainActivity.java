a21ee5de52c90b495cbe0e3021e154fe527d909b
Aristarchos <valeripagian@gmail.com>
2022-11-25 11:03:24 +0200

    create exit key 1.0.7

--- app/src/main/java/com/example/front_end/MainActivity.java
+++ app/src/main/java/com/example/front_end/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.front_end;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button exit;
+
+
+        exit = findViewById(R.id.button_exit);
+
+        exit.setOnClickListener(v -> {
+            finish();
+            System.exit(0);
+        });
     }
 }
