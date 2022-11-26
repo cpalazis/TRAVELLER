793b83067bc9ac208c01ba4b132ff320a7369254
CRISS_PAL <116449160+cpalazis@users.noreply.github.com>
2022-11-26 23:09:53 +0200

    Revert "Grigzigk"

--- app/src/main/java/com/example/front_end/MainActivity.java
+++ app/src/main/java/com/example/front_end/MainActivity.java
@@ -2,23 +2,13 @@ package com.example.front_end;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Intent New=new Intent(getApplicationContext(),Photos.class);
-
-        //startActivity(New);
-
     }
 }
