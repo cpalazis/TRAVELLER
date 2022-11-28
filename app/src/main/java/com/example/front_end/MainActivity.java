5f8de5265730082bc14326e257c45fcf8de6c25d
Giwrgospip <kariothrastis1@hotmail.com>
2022-11-28 10:49:15 +0200

    Connected Top TownsActivity.java to mock api

--- app/src/main/java/com/example/front_end/MainActivity.java
+++ app/src/main/java/com/example/front_end/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.front_end;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button idButtonTown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        idButtonTown = findViewById(R.id.buttonTowns);
+        idButtonTown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent topTowns = new Intent(getApplicationContext(),TownsActivity.class);
+                startActivity(topTowns);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/front_end/SingleCityInfo.java
@@ -0,0 +1,14 @@
+package com.example.front_end;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleCityInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_city_info);
+    }
+}
