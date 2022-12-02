0f6896d865266492f00c8708409fd17f593270bd
Giwrgospip <kariothrastis1@hotmail.com>
2022-12-02 13:28:19 +0200

    Deleted main button

--- app/src/main/java/com/example/front_end/MainActivity.java
+++ app/src/main/java/com/example/front_end/MainActivity.java
@@ -8,20 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button idButtonTown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        idButtonTown = findViewById(R.id.buttonTowns);
-        idButtonTown.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent topTowns = new Intent(getApplicationContext(),TownsActivity.class);
-                startActivity(topTowns);
 
-            }
-        });
     }
 }
