432ac558c27a54f4ee1725918f9653fa809def09
Giwrgospip <kariothrastis1@hotmail.com>
2022-12-02 12:16:02 +0200

    Made Single City Information for every option

--- app/src/main/java/com/example/front_end/RomeActivity.java
+++ app/src/main/java/com/example/front_end/RomeActivity.java
@@ -3,16 +3,14 @@ package com.example.front_end;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
-
-public class SingleCityInfo extends AppCompatActivity {
+public class RomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_city_info);
+        setContentView(R.layout.activity_rome);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 }
