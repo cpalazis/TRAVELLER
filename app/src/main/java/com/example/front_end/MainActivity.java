cbaf958750ef8da5085dc5c3918ae6d11bc9de3b
Aristarchos <valeripagian@gmail.com>
2022-12-12 21:24:53 +0200

    create button for top Town search incomplete 1.1.1

--- app/src/main/java/com/example/front_end/MainActivity.java
+++ app/src/main/java/com/example/front_end/MainActivity.java
@@ -3,13 +3,26 @@ package com.example.front_end;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import com.example.front_end.objects.*;
+import com.example.front_end.utilities.Service;
+import com.example.front_end.utilities.UtilityClass;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button exit, search;
+    private Button exit, search, top;
+    Service service = new Service();
+    Gson gson = new Gson();
+    ArrayList list = new ArrayList();
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -19,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         exit = findViewById(R.id.button_exit);
         search = findViewById(R.id.button_search);
+        top = findViewById(R.id.button_top_cities);
 
         exit.setOnClickListener(v -> {
             finish();
@@ -27,6 +41,28 @@ public class MainActivity extends AppCompatActivity {
 
         search.setOnClickListener(v-> {
 
+        });
+
+        top.setOnClickListener(v -> {
+            service.getJSONArray(this, "topCities", new Service.VolleyResponseListener() {
+                @Override
+                public void onResponse(JSONObject jsonObject) {
+
+                }
+
+                @Override
+                public void onResponse(JSONArray jsonArray) {
+                    list = gson.fromJson(String.valueOf(jsonArray), new TypeToken<ArrayList<Town>>(){}.getType());
+                    UtilityClass.getInstance().setList(list);
+
+
+                }
+
+                @Override
+                public void onError(String message) {
+                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                }
+            });
         });
 
     }
